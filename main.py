9e0c24282f910f03407cd80e26f3aa56fd9f663c
Eshaal2012 <eshaalfarrukh48@gmail.com>
2025-12-06 17:59:43 +0500

    flask mail 2

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import *
+from flask_mail import *
+from random import *
+
+app=Flask(__name__)
+
+app.config["MAIL_SERVER"]='smtp.gmail.com'
+app.config["MAIL_PORT"]=465
+app.config["MAIL_USERNAME"]='eshaalfarrukh48@gmail.com'
+app.config["MAIL_PASSWORD"]='edngdqfwpifqkfwk'
+app.config["MAIL_USE_TLS"]=False
+app.config["MAIL_USE_SSL"]=True
+mail=Mail(app)
+
+otp=randint(000000,999999)
+
+@app.route('/verify',methods=["POST"])
+def verify():
+    email=request.form["email"]
+    msg= Message('OTP',sender='eshaalfarrukh48@gmail.com',recipients=[email])
+    msg.body=str(otp)
+    mail.send(msg)
+    return render_template('page.html')
+
+@app.route('/validate',methods=["POST"])
+def validate():
+    user_otp=request.form["otp"]
+    if otp==int(user_otp):
+        return "<h3>Email  verification is successful</h3>"
+    else:
+        return "<h3>Verification failed ! OTP does not match<h3>"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+app.run(host='0.0.0.0',port=8080,debug=True)
